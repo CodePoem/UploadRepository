dbe3d705f1487a6d1e9f2e98b5bdf33ca1794dc8
CodePoem <806957428@qq.com>
2020-11-17 02:11:28 +0800

    :sparkles: feat(common): add javasourcejar

--- sample/src/main/java/com/vdreamers/sample/MainActivity.kt
+++ sample/src/main/java/com/vdreamers/sample/MainActivity.kt
@@ -3,12 +3,16 @@ package com.vdreamers.sample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vdreamers.demo.lib.android.HelloAndroid
+import com.vdreamers.demo.lib.java.HelloJava
+import com.vdreamers.demo.lib.kotlin.HelloKotlin
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        HelloJava.helloJava()
+        HelloKotlin.helloKotlin()
         HelloAndroid.helloAndroid()
     }
 }
